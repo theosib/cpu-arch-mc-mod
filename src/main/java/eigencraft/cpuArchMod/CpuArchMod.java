b32f18a81521c6b0323e3c9877c36564b82f9ef0
recursive_tree <recursivetreemail@gmail.com>
2020-05-13 16:28:12 +0200

    mod skeleton

---
+++ src/main/java/eigencraft/cpuArchMod/CpuArchMod.java
@@ -0,0 +1,19 @@
+package eigencraft.cpuArchMod;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class CpuArchMod implements ModInitializer {
+
+	
+
+	@Override
+	public void onInitialize() {
+		//Todo
+		System.out.println("Hello!");
+	}
+}
