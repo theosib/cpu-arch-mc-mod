981d2830faf56446d17ca087ea6b704d0716451d
recursive_tree <recursivetreemail@gmail.com>
2020-05-14 12:39:49 +0200

    Revert "Create DataObjectProcessor.java"
    This reverts commit ab42fc03fae02d569c11f1b83e359063811c4358.

--- src/main/java/eigencraft/cpuArchMod/backend/DataObjectProcessor.java
+++
@@ -1,6 +0,0 @@
-package eigencraft.cpuArchMod.backend;
-
-/***All components of this mod should implement this to easily forward a dataObject***/
-public interface DataObjectProcessor {
-    public void processDataObject(AbstractDataObject dataObject);
-}
